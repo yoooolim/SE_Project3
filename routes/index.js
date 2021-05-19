6a4a122875b4c3e6063b97ada4acbd61ca278ddd
Yunhye-Choi <anffl98@gmail.com>
2021-05-19 13:23:23 +0900

    메인피이지 DB연동 완료

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var pool = mysql.createPool({
+  connectionLimit: 5,
+  host:'localhost',
+  user:'root',
+  password: 'anffl!!8623',
+  database:'on_the_board'
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  pool.getConnection(function(err, connection) {
+    connection.query('SELECT * FROM product_tbl ORDER BY sales_amount DESC;', function(err, rows) {
+      if(err) console.error("err: "+err);
+      console.log("rows : "+JSON.stringify(rows));
+
+      res.render('index', {title: 'test', rows : rows});
+      connection.release();
+    });
+  })
 });
 
 module.exports = router;
