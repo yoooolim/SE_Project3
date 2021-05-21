43f3869ee75159faaf59878769b6b4e8eea362e5
Yunhye-Choi <anffl98@gmail.com>
2021-05-21 14:41:24 +0900

    구매페이지 틀 구현 완료

--- routes/index.js
+++ routes/index.js
@@ -22,5 +22,18 @@ router.get('/', function(req, res, next) {
   })
 });
 
+/* 주문 화면 표시 */
+router.get('/order', function(req, res, next) {
+  pool.getConnection(function(err, connection) {
+    connection.query('SELECT * FROM product_tbl;', function(err, rows) {
+      if(err) console.error("err: "+err);
+      console.log("rows : "+JSON.stringify(rows));
+
+      res.render('order', {title: '주문하기', rows : rows});
+      connection.release();
+    });
+  })
+});
+
 module.exports = router;
 
